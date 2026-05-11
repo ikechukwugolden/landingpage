1aca2a14efc370786c3fa08a6812e2b5f2e7a07f
Gerald <madgerald2009@gmail.com>
2026-05-11 17:46:36 +0100

    remove gold stuff

--- src/App.jsx
+++ src/App.jsx
@@ -286,15 +286,14 @@ function Hero() {
               </div>
 
               <div className="visual-status">
-                <div className="status-header">
+                {/* <div className="status-header">
                   <div className="status-dot">
                     <CheckCircle2 className="status-dot-icon" />
                   </div>
-                  <span className="status-label">Gold Standard Events</span>
-                </div>
-                <div className="status-bar">
+                </div> */}
+                {/* <div className="status-bar">
                   <div className="status-fill" />
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
